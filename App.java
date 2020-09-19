06af92b7accec4668bed10d86b55fe0aea2c4597
VamanPundit <71536066+VamanPundit@users.noreply.github.com>
2020-09-19 10:51:57 +0530

    Add files via upload
    Files added.

---
+++ App.java
@@ -0,0 +1,17 @@
+package com.vamandeshmukh.git.demo;
+
+public class App {
+	/** 
+	 * 
+	 * @author Vaman Deshmukh 
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Use this project.");
+		System.out.println("Code changed with this line.");
+
+
+	}
+
+}
